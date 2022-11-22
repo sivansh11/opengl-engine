2549ea16df65dbb0d03ea1a1a339175c33fa539c
sivansh11 <sivanshgupta2002@gmail.com>
2022-11-23 00:08:32 +0530

    .

--- apps/texture_test/main.cpp
+++ apps/texture_test/main.cpp
@@ -50,24 +50,6 @@ void main()
 	FragColor = texture(tex, UVs);
 })";
 
-const char* framebufferVertexShaderSource = R"(#version 460 core
-layout (location = 0) in vec3 pos;
-layout (location = 1) in vec2 uvs;
-out vec2 UVs;
-void main()
-{
-	gl_Position = vec4(2.0 * pos.x, 2.0 * pos.y, 2.0 * pos.z, 1.000);
-	UVs = uvs;
-})";
-const char* framebufferFragmentShaderSource = R"(#version 460 core
-out vec4 FragColor;
-uniform sampler2D screen;
-in vec2 UVs;
-void main()
-{
-	FragColor = vec4(1.0) - texture(screen, UVs);
-})";
-
 
 int main()
 {
@@ -153,49 +135,8 @@ int main()
 	stbi_image_free(bytes);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
-
-	GLuint FBO;
-	glGenFramebuffers(1, &FBO);
-	glBindFramebuffer(GL_FRAMEBUFFER, FBO);
-
-	GLuint framebufferTex;
-	glGenTextures(1, &framebufferTex);
-	glActiveTexture(0);
-	glBindTexture(GL_TEXTURE_2D, framebufferTex);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCREEN_WIDTH, SCREEN_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, framebufferTex, 0);
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	auto fboStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-	if (fboStatus != GL_FRAMEBUFFER_COMPLETE)
-		std::cout << "Framebuffer error: " << fboStatus << "\n";
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-	GLuint framebufferVertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(framebufferVertexShader, 1, &framebufferVertexShaderSource, NULL);
-	glCompileShader(framebufferVertexShader);
-	GLuint framebufferFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(framebufferFragmentShader, 1, &framebufferFragmentShaderSource, NULL);
-	glCompileShader(framebufferFragmentShader);
-
-	GLuint framebufferShaderProgram = glCreateProgram();
-	glAttachShader(framebufferShaderProgram, framebufferVertexShader);
-	glAttachShader(framebufferShaderProgram, framebufferFragmentShader);
-	glLinkProgram(framebufferShaderProgram);
-
-	glDeleteShader(framebufferVertexShader);
-	glDeleteShader(framebufferFragmentShader);
-
-
 	while (!glfwWindowShouldClose(window))
 	{
-		glBindFramebuffer(GL_FRAMEBUFFER, FBO);
 		glClearColor(19.0f / 255.0f, 34.0f / 255.0f, 44.0f / 255.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
@@ -204,15 +145,6 @@ int main()
 		glBindTexture(GL_TEXTURE_2D, tex);
 		glUniform1i(glGetUniformLocation(shaderProgram, "tex"), 0);
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(indices[0]), GL_UNSIGNED_INT, 0);
-
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-		glUseProgram(framebufferShaderProgram);
-		glActiveTexture(0);
-		glBindTexture(GL_TEXTURE_2D, framebufferTex);
-		glUniform1i(glGetUniformLocation(framebufferShaderProgram, "screen"), 0);
-		glBindVertexArray(VAO); // NO framebuffer VAO because I simply double the size of the rectangle to cover the whole screen
 		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(indices[0]), GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
